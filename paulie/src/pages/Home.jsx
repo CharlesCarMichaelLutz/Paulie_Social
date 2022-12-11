a58f7e218d137f35886cef0f6ddef547f5ce57bf
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-12-11 11:31:04 +0800

    User Card

--- paulie/src/pages/Home.jsx
+++ paulie/src/pages/Home.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { useState } from 'react';
-
+import image from '../bigparrot.jpg';
 const Home = () => {
 
   return (
-    <div>This is the Home Page of Paulie Social!</div>
+    <body>
+      <header>
+      Paulie Social!
+      </header>
+
+      <main>
+
+        <span>
+          Lorem ipsum dolor sit amet. Et repellat 
+          obcaecati in molestiae illo ut architecto 
+          eius aut doloribus sint. Non explicabo dolorem 
+          id quia adipisci ea quia quae quo quia doloribus
+          et quibusdam molestiae non laborum eveniet. Est
+          fuga dolores qui dolores vitae aut corporis tempora 
+          cum nihil quae sed corporis eveniet.         
+        </span>
+
+        <span>
+          <img src={image} height={400} width={400} />
+        </span>
+      </main>
+
+    </body>
   )
 }
 
--- paulie/src/pages/UserCard.jsx
+++ paulie/src/pages/UserCard.jsx
@@ -4,19 +4,23 @@ import { CCardBody } from '@coreui/react';
 import { CCardTitle } from '@coreui/react';
 import { CCardSubtitle } from '@coreui/react';
 import { CCardImage } from '@coreui/react';
+import { CContainer } from '@coreui/react';
 
 const UserCard = ({user}) => {
 
   let {username, userhandle, userImage} = user
   return (
-    <div>
-    <CCard style={{width: '18rem'}}>
-      <CCardBody>
-        <CCardImage orientation="top" src={userImage} />
-        <CCardTitle>{username}</CCardTitle>
-        <CCardSubtitle>{userhandle}</CCardSubtitle>
-      </CCardBody>
-    </CCard>
+    <div className='user--grid'>
+    <CContainer className='user--cards' >
+        <CCard style={{width: '18rem'}}>
+          <CCardBody>
+            <CCardImage orientation="top" src={userImage} />
+            <CCardTitle>{username}</CCardTitle>
+            <CCardSubtitle>{userhandle}</CCardSubtitle>
+          </CCardBody>
+        </CCard>    
+    </CContainer>  
+   
     </div>
   )
 }
