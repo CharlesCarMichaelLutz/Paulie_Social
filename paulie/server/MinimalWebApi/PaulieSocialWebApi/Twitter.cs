d409cef3dfa0cae293ec26fe038bcc680451ed2f
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2023-03-18 09:10:58 +0800

    added the Twitter class

---
+++ paulie/server/MinimalWebApi/PaulieSocialWebApi/Twitter.cs
@@ -0,0 +1,12 @@
+﻿
+namespace PaulieSocialWebApi
+{
+    public class Twitter
+    {
+        public string TweetContent { get; set; }
+        public string AuthorId { get; set; }
+        public string MediaKey { get; set; }
+        public int RetweetCount { get; set; }
+        public int HeartedCount { get; set; }
+    }
+}
