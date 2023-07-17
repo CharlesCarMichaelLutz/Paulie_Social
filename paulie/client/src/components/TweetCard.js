d48f1a5a34a2a8729080003688ab6aadf5745d9e
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2023-07-17 17:57:00 +0800

    updated TweetModel.cs to include variants array and url for media.fields

--- paulie/client/src/components/TweetCard.js
+++ paulie/client/src/components/TweetCard.js
@@ -7,12 +7,12 @@ const TweetCard = ({tweetList}) => {
 
   return (
         <div key={id} className="card-container">
-            <h5 className="card-title">{id}</h5>
-            <h6 className="card-title">{author_id}</h6>
+            <h5 className="card-title">{id}</h5> {/* profile_image_url */}
+            <h6 className="card-title">{author_id}</h6> {/* username */}
             <p className="card-text">{text}</p>
             <p>{public_metrics.like_count}</p>
             <p>{public_metrics.retweet_count}</p>
-            {media && <Media mediaData={media}/>}
+            {media && <Media mediaData={media}/>} {/* media.url*/}
         </div>
   )
 }
--- paulie/server/MinimalWebApi/PaulieSocialWebApi/Models/TweetModel/TweetModel.cs
+++ paulie/server/MinimalWebApi/PaulieSocialWebApi/Models/TweetModel/TweetModel.cs
@@ -8,7 +8,7 @@ namespace PaulieSocialWebApi.Models.TweetModel
     public class TweetModel
     {
         public TweetData[] Data { get; set; }
-        public includes includes { get; set; } 
+        public includes Includes { get; set; }
     }
     public class TweetData
     {
@@ -18,34 +18,87 @@ namespace PaulieSocialWebApi.Models.TweetModel
         public public_metrics public_metrics { get; set; }
         public attachments attachments { get; set; }
     }
-
     public class public_metrics
     {
         public int retweet_count { get; set; }
         public int like_count { get; set; }
     }
-
     public class attachments
     {
         public string[] media_keys { get; set; }
     }
-
     public class includes
     {
-        public media[] media { get; set; }
-        public users[] users { get; set; } 
+        public media[] Media { get; set; }
+        public users[] Users { get; set; }
     }
-
     public class media
     {
-        public string type { get; set; }
-        public string media_key { get; set; }
+        public string Type { get; set; }
+        public string Media_key { get; set; }
+        public string url { get; set; }
+        public variants[] Variants { get; set; }
     }
 
+    public class variants
+    {
+        public string url { get; set; }
+    }
     public class users
     {
         public string username { get; set; }
         public string profile_image_url { get; set; }
     }
+
+    //public class TweetModel
+    //{
+    //    public TweetData[] Data { get; set; }
+    //    public Includes Includes { get; set; }
+    //}
+
+    //public class TweetData
+    //{
+    //    public string author_id { get; set; }
+    //    public string text { get; set; }
+    //    public string id { get; set; }
+    //    public PublicMetrics public_metrics { get; set; }
+    //    public Attachments attachments { get; set; }
+    //}
+
+    //public class PublicMetrics
+    //{
+    //    public int retweet_count { get; set; }
+    //    public int like_count { get; set; }
+    //}
+
+    //public class Attachments
+    //{
+    //    public Media[] media_keys { get; set; }
+    //}
+
+    //public class Includes
+    //{
+    //    public Media[] Media { get; set; }
+    //    public User[] Users { get; set; }
+    //}
+
+    //public class Media
+    //{
+    //    public string Type { get; set; }
+    //    public string Media_key { get; set; }
+    //    public string url { get; set; }
+    //    public Variants[] Variants { get; set; }
+    //}
+
+    //public class Variants
+    //{
+    //    public string url { get; set; }
+    //}
+
+    //public class User
+    //{
+    //    public string username { get; set; }
+    //    public string profile_image_url { get; set; }
+    //}
 }
 
